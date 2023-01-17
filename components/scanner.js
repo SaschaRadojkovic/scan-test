32fddd3293edbcd546530ddaabd8cbff2457b8fa
sascha radojkovic <scha13@gmx.de>
2023-01-17 19:54:09 +0100

    use back cam

--- components/scanner.js
+++ components/scanner.js
@@ -37,7 +37,7 @@ const Scanner = ({
   scannerRef,
   onScannerReady,
   cameraId,
-  facingMode,
+  facingMode = "environment",
   constraints = defaultConstraints,
   locator = defaultLocatorSettings,
   numOfWorkers = 0,
